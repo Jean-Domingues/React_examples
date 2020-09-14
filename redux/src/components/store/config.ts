abefc5984ce4144a5684ff67d55fba111e5af7b0
Jean Domingues <jeandomingues752@gmail.com>
2020-09-14 20:18:20 -0300

    Configurando teste simples

---
+++ redux/src/components/store/config.ts
@@ -0,0 +1,14 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: 0,
+  reducers: {
+    increment: state => state + 1,
+    decrement: state => state - 1
+  }
+})
+
+export const store = configureStore({
+  reducer: counterSlice.reducer
+})
