2db0a72fdd3bf6f82d24e14d548b09c142778e41
Jean Domingues <jeandomingues752@gmail.com>
2021-03-08 22:47:38 -0300

    Estilizando o formulário

--- react_formik/src/components/Form/index.tsx
+++ react_formik/src/components/Form/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 
+import styles from '../../style/Form.module.css';
+
 export const SignupForm = () => {
   return (
     <Formik
@@ -22,32 +24,41 @@ export const SignupForm = () => {
         }, 400);
       }}
     >
-      {props => ( // Função que será passada para o contexto do Formik
-        <form onSubmit={props.handleSubmit}>
+      {(
+        props // Função que será passada para o contexto do Formik
+      ) => (
+        <form onSubmit={props.handleSubmit} className={styles.container}>
           <label htmlFor="firstName">First Name</label>
           <input
+            className={styles.inputForm}
             id="firstName"
             type="text"
             {...props.getFieldProps('firstName')}
           />
           {props.touched.firstName && props.errors.firstName ? (
-            <div>{props.errors.firstName}</div>
+            <div className={styles.errors}>{props.errors.firstName}</div>
           ) : null}
 
           <label htmlFor="lastName">Last Name</label>
           <input
+            className={styles.inputForm}
             id="lastName"
             type="text"
             {...props.getFieldProps('lastName')}
           />
           {props.touched.lastName && props.errors.lastName ? (
-            <div>{props.errors.lastName}</div>
+            <div className={styles.errors}>{props.errors.lastName}</div>
           ) : null}
 
           <label htmlFor="email">Email Address</label>
-          <input id="email" type="email" {...props.getFieldProps('email')} />
+          <input
+            className={styles.inputForm}
+            id="email"
+            type="email"
+            {...props.getFieldProps('email')}
+          />
           {props.touched.email && props.errors.email ? (
-            <div>{props.errors.email}</div>
+            <div className={styles.errors}>{props.errors.email}</div>
           ) : null}
 
           <button type="submit">Submit</button>
--- react_formik/src/pages/Home/Home.tsx
+++ react_formik/src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { SignupForm } from '../../components/Form'
 
+import styles from '../../style/Home.module.css'
+
 export function Home () {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Preencha o formulário:</h1>
       <SignupForm />
     </div>
