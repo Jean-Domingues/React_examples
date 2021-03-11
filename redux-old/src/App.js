053a10782e7761fb8edc83ad5dfc2ec50ed0a976
Jean Domingues <jeandomingues752@gmail.com>
2021-03-11 00:07:08 -0300

    Adicionando outra action

--- redux-old/src/App.js
+++ redux-old/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import { connect } from 'react-redux'
 
-function App({ counter, increment }) {
+function App({ counter, increment, decrement }) {
   return (
     <div>
       <h1>Contador: {counter}</h1>
       <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </div>
   );
 }
@@ -19,7 +20,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    increment: () => dispatch({ type: 'counter/incremented' })
+    increment: () => dispatch({ type: 'counter/incremented' }),
+    decrement: () => dispatch({ type: 'counter/decremented'})
   }
 }
 
