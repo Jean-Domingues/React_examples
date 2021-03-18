0c584ba5313c75b4429db00818a8d4cfb4076ef3
Jean Domingues <jeandomingues752@gmail.com>
2021-03-17 23:59:41 -0300

    Adicionando mais comentários

--- redux-saga/src/store/index.js
+++ redux-saga/src/store/index.js
@@ -4,13 +4,14 @@ import { counterReducer } from './reducer'
 
 import mySaga from './sagas/index'
 
+// Cria o middleware para interceptar ações do nosso app
 const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(
   counterReducer,
-  applyMiddleware(sagaMiddleware)
+  applyMiddleware(sagaMiddleware) // aplica o middleware na nossa loja
 )
 
-sagaMiddleware.run(mySaga)
+sagaMiddleware.run(mySaga) // Roda o middleware passando como parâmetro o nosso saga
 
 export default store;
--- redux-saga/src/store/sagas/index.js
+++ redux-saga/src/store/sagas/index.js
@@ -1,12 +1,15 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import { getData } from "../../utils/getJsonValue";
 
-function* fetchUser() {
-  const counterNewValue = yield call(getData);
+function* fetchUser() { // TRATATIVA
+  // pega o dado e retorna em uma nova action do redux
+  const counterNewValue = yield call(getData); 
   yield put({ type: "counter/async_increment", value: counterNewValue });
 }
 
-function* mySaga() {
+function* mySaga() { // Função responsárvel por receber as actions e disparar as tratativas
+
+  // vai pegar a nossa última chamada para a action, e dar uma tratativa
   yield takeLatest("counter/async_request", fetchUser);
 }
 
